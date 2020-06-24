f188c182a729c2cbf51513ddb09487bc7e2f7e3c
lcdr <lcdr@users.noreply.github.com>
2020-06-24 11:51:22 +0200

    Make server-sent messages deserializable

--- src/general/client.rs
+++ src/general/client.rs
@@ -1,13 +1,12 @@
 use std::io::Result as Res;
 
-use endio::{LEWrite, Serialize};
-use endio::LittleEndian as LE;
+use endio::{Deserialize, LE, LERead, LEWrite, Serialize};
 
 use crate::common::ServiceId;
 
 pub type Message<C> = crate::raknet::client::Message<LuMessage<C>>;
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Deserialize, Serialize)]
 #[repr(u16)]
 pub enum LuMessage<C> {
 	General(GeneralMessage) = ServiceId::General as u16,
@@ -26,7 +25,7 @@ impl<C> From<GeneralMessage> for Message<C> {
 	}
 }
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Deserialize, Serialize)]
 #[post_disc_padding=1]
 #[repr(u32)]
 pub enum GeneralMessage {
@@ -52,11 +51,24 @@ pub struct Handshake {
 	pub service_id: ServiceId,
 }
 
+impl<R: LERead> Deserialize<LE, R> for Handshake
+	where   u16: Deserialize<LE, R>,
+	        u32: Deserialize<LE, R>,
+	  ServiceId: Deserialize<LE, R> {
+	fn deserialize(reader: &mut R) -> Res<Self> {
+		let network_version = reader.read()?;
+		let _: u32 = reader.read()?;
+		let service_id = reader.read()?;
+		let _: u16 = reader.read()?;
+		Ok(Self { network_version, service_id })
+	}
+}
+
 impl<'a, W: LEWrite> Serialize<LE, W> for &'a Handshake
 	where       u16: Serialize<LE, W>,
 	            u32: Serialize<LE, W>,
 	  &'a ServiceId: Serialize<LE, W> {
-	fn serialize(self, writer: &mut W) -> Res<()>	{
+	fn serialize(self, writer: &mut W) -> Res<()> {
 		writer.write(self.network_version)?;
 		writer.write(0u32)?;
 		writer.write(&self.service_id)?;
@@ -65,7 +77,7 @@ impl<'a, W: LEWrite> Serialize<LE, W> for &'a Handshake
 	}
 }
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Deserialize, Serialize)]
 #[repr(u32)]
 pub enum DisconnectNotify {
 	UnknownServerError,
--- src/raknet/client.rs
+++ src/raknet/client.rs
@@ -1,10 +1,8 @@
-use std::io::Result as Res;
 use std::net::Ipv4Addr;
 
-use endio::{LEWrite, Serialize};
-use endio::LittleEndian as LE;
+use endio::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Deserialize, Serialize)]
 #[non_exhaustive]
 #[repr(u8)]
 pub enum Message<U> {
@@ -26,28 +24,17 @@ impl<U> From<ConnectionRequestAccepted> for Message<U> {
 	}
 }
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct ConnectedPong {
 	pub ping_send_time: u32,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Deserialize, Serialize)]
 pub struct ConnectionRequestAccepted {
 	pub peer_ip: Ipv4Addr,
 	pub peer_port: u16,
+	#[padding=2]
 	pub local_ip: Ipv4Addr,
 	pub local_port: u16,
 }
 
-impl<'a, W: LEWrite> Serialize<LE, W> for &'a ConnectionRequestAccepted
-	where       u16: Serialize<LE, W>,
-	            u32: Serialize<LE, W>,
-	   &'a Ipv4Addr: Serialize<LE, W> {
-	fn serialize(self, writer: &mut W) -> Res<()>	{
-		writer.write(&self.peer_ip)?;
-		writer.write(self.peer_port)?;
-		writer.write(0u16)?;
-		writer.write(&self.local_ip)?;
-		writer.write(self.local_port)
-	}
-}
