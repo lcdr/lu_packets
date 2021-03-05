582092cb5f5db82f4227ca0ad896727c1b1a94c3
lcdr <lcdr@users.noreply.github.com>
2021-03-05 11:28:35 +0100

    Fix rigid body phantom physics unit test

--- src/raknet/client/replica/tests/RigidBodyPhantomPhysicsConstruction.rs
+++ src/raknet/client/replica/tests/RigidBodyPhantomPhysicsConstruction.rs
@@ -1,12 +1,12 @@
 RigidBodyPhantomPhysicsConstruction {
 	position_rotation_info: Some(
 		PositionRotationInfo {
-			position: Vector3 {
+			position: crate::world::Vector3 {
 				x: 6.0,
 				y: 7.0,
 				z: 8.0,
 			},
-			rotation: Quaternion {
+			rotation: crate::world::Quaternion {
 				x: 0.0,
 				y: 0.53361946,
 				z: 0.0,
